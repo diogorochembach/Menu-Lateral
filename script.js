55c0906d3361a0020c85e370a529274815f22613
diogorochembach <diogo.iju@gmail.com>
2023-04-06 15:30:46 -0300

    Finalização codigo javascript

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ function activeLink() {
   list.forEach((item) => item.classList.remove("active"));
   this.classList.add("active");
 }
+list.forEach((item) => item.addEventListener("click", activeLink));
